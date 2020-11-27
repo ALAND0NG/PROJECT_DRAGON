8570bab272f61256bfde7722ca3fd8875b8b0adb
Noah Glassford <noahglassford@gmail.com>
2020-11-26 22:08:24 -0500

    improved asset loader

--- modules/Element_Engine/include/AssetLoader.h
+++ modules/Element_Engine/include/AssetLoader.h
@@ -4,9 +4,15 @@
 static class AssetLoader
 {
 public:
+	static void Init();
 	static std::vector<Mesh>& GetMesh();
 	static std::vector<Material>& GetMat();
+	static Mesh& GetMeshFromStr(std::string name);
+	static Material& GetMatFromStr(std::string name);
+
 private:
-	static std::vector<Mesh> m_Meshes; //k basically just fucking memorize it im not making a full system to track this shit
+	static std::vector<Mesh> m_Meshes;
 	static std::vector<Material> m_Materials;
+	static std::vector<std::string> m_MeshNames;
+	static std::vector<std::string> m_MaterialNames;
 };
