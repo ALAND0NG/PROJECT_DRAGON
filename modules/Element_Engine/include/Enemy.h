dc3ba479bb3e109273e776166b45dc0282dfb1b7
Clark <clark.perritt@ontariotechu.net>
2020-12-01 18:05:41 -0500

    Shit is Broken

--- modules/Element_Engine/include/Enemy.h
+++ modules/Element_Engine/include/Enemy.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <ECS.h>
+#include <ElementEngine.h>
+#include <AssetLoader.h>
 #include <Timer.h>
 //basically playerdata but for the enemy
 class Enemy
@@ -15,6 +18,7 @@ public:
 	btVector3 thisPosition;
 	btVector3 distance;
 	int entityNumber = 0;
+	float distanceNorm = 100.0f;
 	float mTimer = 0.0f;
 
 	//External Values
@@ -26,11 +30,24 @@ public:
 		if (mTimer >= 2) { //Update general enemy information every 2 seconds to reduce computing
 			thisPosition = ECS::Get<PhysicsBody>(entityNumber).GetBody()->getCenterOfMassTransform().getOrigin();
 			playerPosition = ECS::Get<PhysicsBody>(0).GetBody()->getCenterOfMassTransform().getOrigin();
-			distance.setX(sqrtf(powf(thisPosition.getX() - playerPosition.getX(), 2.0f)));
-			distance.setY(sqrtf(powf(thisPosition.getY() - playerPosition.getY(), 2.0f)));
-			distance.setZ(sqrtf(powf(thisPosition.getZ() - playerPosition.getZ(), 2.0f)));
+			distance.setX(powf(thisPosition.getX() - playerPosition.getX(), 2.0f));
+			distance.setY(powf(thisPosition.getY() - playerPosition.getY(), 2.0f));
+			distance.setZ(powf(thisPosition.getZ() - playerPosition.getZ(), 2.0f));
 
 			std::cout << "\nX: " << distance.getX() << "\nY: " << distance.getY() << "\nZ: " << distance.getZ() << "\n\n";
+			distanceNorm = sqrtf(distance.getX() + distance.getY() + distance.getZ());
+
+			//Chance behavior of enemy based on distance
+
+			//Very Close
+
+			//
+
+			//Close
+
+			//Mid Range
+
+			//Long Range
 		}
 	}
 };
